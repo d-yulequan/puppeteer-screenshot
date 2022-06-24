acecc2c9a6c83254e998ef825600797f49e9c2f8
JunyWuuuu91 <564109763@qq.com>
2022-06-24 14:03:45 +0800

    fix: valid url format

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
-const Koa = require('koa')
-const app = new Koa()
+const Koa = require("koa");
+const app = new Koa();
 
-app.use(async ctx => {
-  if (ctx.request.url.indexOf('/') > -1) {
-    // TODO
-  } else {
-    ctx.body = '链接错了，傻蛋'
+app.use(async (ctx) => {
+  const { request } = ctx;
+  let url;
+
+  try {
+    url = new URL(request?.url);
+  } catch (error) {
+    ctx.body = "链接错了，傻蛋";
   }
+
+  // TODO
 });
 
 app.listen(3011);
-console.info(`server is running at 3011`)
+console.info(`server is running at 3011`);
