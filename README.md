ac40a4ab03b42f396083dd5f74dbf595d8467bf0
王 煜野 <wangyuye@dxy.cn>
2022-06-30 10:32:39 +0800

    chore: 完善一下

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 基于 puppeteer 截取指定页面。
 过程中会发生自顶向下的滚动，和去除带有一定特征的遮挡浮窗。
+
+<video controls preload="none" poster="demo">
+  <source src="demo.mp4" type="video/mp4">
+</videos>
--- src/screenshot.js
+++ src/screenshot.js
@@ -1,23 +1,23 @@
 const puppeteer = require("puppeteer");
 const { autoScroll, modalKiller, checkBodyScroll } = require("./utils");
-const rimraf = require("rimraf");
 const fs = require("fs");
 const { resolve } = require("path");
 
 function createDir(dirname) {
   const dir = resolve(__dirname, "..", dirname);
-  fs.existsSync(dir)
-    ? rimraf(dir, () => {
-        console.log("删除", dirname);
-        fs.mkdirSync(dir);
-      })
-    : fs.mkdirSync(dir);
+  if (fs.existsSync(dir)) {
+    fs.rmSync(dir, { recursive: true });
+    console.info(`存在/${dirname}，删除下`, `删除/${dirname}√√√`);
+    fs.mkdirSync(dir);
+    console.info("重新生成", `${dirname}√√√`);
+  } else {
+    fs.mkdirSync(dir);
+    console.info("生成", `/${dirname}√√√`);
+  }
 }
-createDir("shots");
-createDir("pdf");
 
 // 对单个页面取快照
-const screenshot = async (page, url) => {
+const screenshot = async (page, url, type) => {
   return new Promise((resolve) => {
     page
       .goto(url, {
@@ -29,12 +29,15 @@ const screenshot = async (page, url) => {
 
         setTimeout(async () => {
           await modalKiller(page);
-          const filename = async (dirname, type) => `./${dirname}/${await page.title()}.${type}`;
-          await page.screenshot({
-            fullPage: true,
-            path: await filename("shots", "png"),
-          });
-          await page.pdf({ path: await filename("pdf", "pdf"), format: "a4" });
+          const filename = async (dirname) => `./${dirname}/${await page.title()}.${type}`;
+
+          type === "pdf"
+            ? await page.pdf({ path: await filename("pdf", type), format: "a4" })
+            : await page.screenshot({
+                fullPage: true,
+                path: await filename("shots", type),
+              });
+
           await page.close();
           resolve();
         }, 1000);
@@ -48,7 +51,7 @@ const screenshot = async (page, url) => {
 };
 
 // 启动应用
-const run = async (urls = []) => {
+const run = async (urls = [], type = "png") => {
   if (!Array.isArray(urls) || !urls.length) {
     return;
   }
@@ -64,10 +67,13 @@ const run = async (urls = []) => {
     ],
     timeout: 0,
     pipe: true,
-    headless: true,
+    headless: false,
     ignoreHTTPSErrors: true,
     defaultViewport: null,
   });
+
+  // 初始化存储文件夹
+  createDir(type === "pdf" ? "pdf" : "shots");
 
   const shots = [];
   for await (const url of urls) {
@@ -75,7 +81,7 @@ const run = async (urls = []) => {
     await page.setUserAgent(
       "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.115 Safari/537.36"
     );
-    shots.push(screenshot(page, url));
+    shots.push(screenshot(page, url, type));
   }
 
   await Promise.all(shots);
